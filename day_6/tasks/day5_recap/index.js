7d7b0f2a28fd15243ec051c7cd7cb9ad1d5a2348
Mateusz Zagórski <mateuszzagorski1@gmail.com>
2023-04-04 13:39:08 +0200

    Day5_recap tasks added.

---
+++ day_6/tasks/day5_recap/index.js
@@ -0,0 +1,41 @@
+// 1. Zmiana funkcji standardowej na funkcję strzałkową:
+// function multiplyByTwo(numbers) {
+// 	let multipliedNumbers = [];
+
+// 	for (let i = 0; i < numbers.length; i++) {
+// 		multipliedNumbers.push(numbers[i] * 2);
+// 	}
+
+// 	return multipliedNumbers;
+// }
+// console.log(multiplyByTwo([1, 2, 3, 4, 5]));
+
+// 2. Zmiana funkcji standardowej na funkcję strzałkową:
+// function getFirstLetter(str) {
+// 	return str.charAt(0);
+// }
+
+// 3. Zmiana funkcji standardowej na funkcję strzałkową:
+// function condition(num) {
+// 	if (num >= 3) {
+// 		return num;
+// 	}
+// }
+
+// function filterNumbers(numbers, condition) {
+// 	let filteredNumbers = [];
+
+// 	for (let i = 0; i < numbers.length; i++) {
+// 		if (condition(numbers[i])) {
+// 			filteredNumbers.push(numbers[i]);
+// 		}
+// 	}
+
+// 	return filteredNumbers;
+// }
+
+// console.log(filterNumbers([1, 2, 3, 4, 5, 6], condition));
+
+// 4 stwórz klasę Person która po wywołaniu metody greet() pokaże w konsoli to co poniżej:
+// let john = new Person("John", 25);
+// john.greet(); // Hello, my name is John and I am 25 years old.
