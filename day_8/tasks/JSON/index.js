a1bf8954383474b55c24b847a4d2e8144ce99550
Mateusz Zagórski <mateuszzagorski1@gmail.com>
2023-04-06 16:21:38 +0200

    Added day8 tasks.

---
+++ day_8/tasks/JSON/index.js
@@ -0,0 +1,18 @@
+//
+// 1. Zamień użytkownika w JSON, a następnie odczytaj go z powrotem do innej zmiennej.
+//
+// const user = {
+// 	name: "Jan",
+// 	surname: "Kowalski",
+// 	age: 30,
+// 	email: "jan.kowalski@example.com",
+// };
+
+//
+// 2. Zamień tablicę obiektów w JSON, a następnie odczytaj ją z powrotem do innej zmiennej
+//
+// const cars = [
+// 	{ brand: "Toyota", model: "Corolla", year: 2020 },
+// 	{ brand: "Honda", model: "Civic", year: 2018 },
+// 	{ brand: "Ford", model: "Mustang", year: 2015 },
+// ];
---
+++ day_8/tasks/OOP/index.js
@@ -0,0 +1,3 @@
+//
+// 1. Stwórz klasę Vehicle, która będzie miała właściwości make (marka) i model (model), oraz metodę honk (trąbienie). Następnie stwórz klasę Car, która będzie dziedziczyła po klasie Vehicle i będzie miała dodatkową właściwość year (rok produkcji) oraz metodę drive (jazda). W metodzie drive wypisz w konsoli napis "Jadę swoim <marka> <model>".
+//
